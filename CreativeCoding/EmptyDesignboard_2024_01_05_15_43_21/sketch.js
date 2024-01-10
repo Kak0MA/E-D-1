2b7af8fcbfd8ce83732333484e47c27719de06a6
Kak0MA <mkakoma@brunswickschool.org>
2024-01-10 10:23:35 -0500

    update

--- CreativeCoding/EmptyDesignboard_2024_01_05_15_43_21/sketch.js
+++ CreativeCoding/EmptyDesignboard_2024_01_05_15_43_21/sketch.js
@@ -1,8 +1,30 @@
-function setup() {//runs one time
-createCanvas (windowWidth, windowHeight);//canvas sized to window
-background (150, 0, 0);//red background
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background('gray') 
+  strokeWeight(1);
+  fill('coral');
 }
 
-function draw () {//runs in a loop
+function draw() {
+  
+  var num = 3;
+  var sideLen = windowWidth/num;
+  
+ for(var y = 0; y < windowHeight; y = y + sideLen ){ 
+  for(var x = 0; x < windowWidth; x = x + sideLen){
+    
+   quad (
 
+   x,y,
+   x + sideLen,y,
+   x + sideLen,360 + y ,
+   x,sideLen + y
+);   
+  }
+ }
+}
+
+function windowResized(){
+  
+  resizeCavas(windowWidth,windowHeight)
 }
