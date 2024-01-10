5d533cfcfb51921c9131c8d514f184cd8fddacbe
Kak0MA <mkakoma@brunswickschool.org>
2024-01-10 10:39:00 -0500

    variables

---
+++ CreativeCoding/Variables/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background('gray') 
+  strokeWeight(1);
+  fill('coral');
+}
+
+function draw() {
+  
+  var num = 3;
+  var sideLen = windowWidth/num;
+  
+ for(var y = 0; y < windowHeight; y = y + sideLen ){ 
+  for(var x = 0; x < windowWidth; x = x + sideLen){
+    
+   quad (
+
+   x,y,
+   x + sideLen,y,
+   x + sideLen,360 + y ,
+   x,sideLen + y
+);   
+  }
+ }
+}
+
+function windowResized(){
+  
+  resizeCavas(windowWidth,windowHeight)
+}
