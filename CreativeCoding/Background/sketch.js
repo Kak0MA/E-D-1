2403efb2fc0836b0bfacf51e345a721b9090ea48
Kak0MA <mkakoma@brunswickschool.org>
2024-01-11 13:58:17 -0500

    Final Background

--- CreativeCoding/Background/sketch.js
+++ CreativeCoding/Background/sketch.js
@@ -1,8 +1,35 @@
-function setup() {//runs one time
-createCanvas (windowWidth, windowHeight);
- background('red');
+
+let gif;
+
+function preload(){
+gif = loadImage ('Assets/GIF.gif');
 }
 
-function draw() {//runs in a loop
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background('gray') 
+  strokeWeight(1);
+  fill('coral');
+}
 
+function draw() {
+  
+  background('coral');
+  
+  var num = 3;
+  var sideLen = windowWidth/num;
+  
+ for(var y = 0; y < windowHeight; y = y + sideLen ){ 
+  for(var x = 0; x < windowWidth; x = x + sideLen){
+
+imag(gif,x,y,sideLen,sideLen);  
+
+ 
+  }
+ }
+}
+
+function windowResized(){
+  
+  resizeCavas(windowWidth,windowHeight);
 }
---
+++ CreativeCoding/Background3/sketch.js
@@ -0,0 +1,8 @@
+function setup() {//runs one time
+createCanvas (windowWidth, windowHeight);
+ background('red');
+}
+
+function draw() {//runs in a loop
+
+}
