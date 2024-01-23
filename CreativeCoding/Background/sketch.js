2c168abf2125503cbcb85bd51806b32689a5090f
Kak0MA <mkakoma@brunswickschool.org>
2024-01-23 10:40:26 -0500

    changes

--- CreativeCoding/Background/sketch.js
+++ CreativeCoding/Background/sketch.js
@@ -3,21 +3,28 @@ let gif;
 
 function preload(){
 gif = loadImage ('Assets/GIFFinal.gif');
-}
 
+}
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  fill('lightgrey');
+  stroke('white');
+  strokeWeight(1);
+  
 }
 
 function draw() {
   
   background('coral');
   
-  var num = 3;
+  var num = 3
+  var sideLen = windowWidth
+  
   var sideLen = windowWidth/num;
   
- for(var y = 0; y < windowHeight; y = y + sideLen ){ 
-  for(var x = 0; x < windowWidth; x = x + sideLen){
+     for(var y = 0; y < windowHeight; y = y + sideLen ) { 
+     
+        for(var x = 0; x < windowWidth; x = x + sideLen) {
 
 image(gif,x,y,sideLen,sideLen);  
 
