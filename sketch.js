3612999fb77c53c9ea25b518da84b169bd3a6e31
Kak0MA <mkakoma@brunswickschool.org>
2024-01-25 10:47:10 -0500

    changes

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+let gif;
+
+function preload(){
+gif = loadImage ('Assets/GifEnd.gif');
+
+}
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  fill('lightgrey');
+  stroke('white');
+  strokeWeight(1);
+  
+}
+
+function draw() {
+  
+  background('coral');
+  
+  var num = 3
+  var sideLen = windowWidth
+  
+  var sideLen = windowWidth/num;
+  
+     for(var y = 0; y < windowHeight; y = y + sideLen ) { 
+     
+        for(var x = 0; x < windowWidth; x = x + sideLen) {
+
+image(gif,x,y,sideLen,sideLen);  
+
+ 
+  }
+ }
+}
+
+function windowResized(){
+  
+  resizeCavas(windowWidth,windowHeight);
+}
